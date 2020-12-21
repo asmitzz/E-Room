e2901e024a2f21d61d0eee7394ca91b4a32ec314
Asmit shrivastava <asmitshrivastava8@gmail.com>
2020-12-21 19:45:33 +0530

    feat: added posts

--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -1,11 +1,12 @@
 import React,{ Component } from 'react';
 import Nav from '../navigation/navigation';
-
+import Posts from './posts/posts';
 class Home extends Component{
   render(){
     return(
         <div> 
            <Nav/>
+           <Posts />
         </div>
     );
   };
