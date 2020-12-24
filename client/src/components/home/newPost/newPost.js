bb98c0546fd7a7828b0a6e2087ea5b50be9dc74b
Asmit shrivastava <asmitshrivastava8@gmail.com>
2020-12-25 00:48:00 +0530

    added validation

--- client/src/components/home/newPost/newPost.js
+++ client/src/components/home/newPost/newPost.js
@@ -21,7 +21,9 @@ class newPost extends Component{
        image2:"",
        image3:"",
        image4:"",
-       error:""
+       error:"",
+       isphonetrue:"",
+       ispintrue:""
     };
 
     nameHandler = (e) => {
@@ -98,32 +100,46 @@ class newPost extends Component{
         }
 
         uploadBtnDisable = true;
+        this.setState({error:""})
 
     }
 
-    submitHandler = () => {
-        axios.post('http://localhost:8000/api/insert',this.state);
-        console.log(this.state);
-        this.props.history.push('/');
+    submitHandler = (event) => {
+        event.preventDefault();
+        if(!uploadBtnDisable){
+            return;
+        }
+        else if( this.state.pincode.length !== 6 ){
+            this.setState({ ispintrue:"please enter valid pincode" })
+        }
+        else if( this.state.phone.length === 10 ){
+            axios.post('http://localhost:8000/api/insert',this.state);
+            this.props.history.push('/');
+        }
+        else{
+            this.setState({ ispintrue:"" })
+            this.setState({ isphonetrue:"please enter valid number" })
+        }
+            
     }
 
 
     render(){
-
+        console.log(this.state.pincode.length)
         return(
             <div>
                 <Nav/>
-                <div className="container">
+                <div className="container my-3">
                 <h1 className="display-1 contact-us">E-Room</h1>
-                
+                 <form>
                     <div className="form-group">
                       <label>Enter Name:</label>
-                      <input type="text" onChange={this.nameHandler} className="form-control" placeholder="Enter your name.."/>
+                      <input type="text" onChange={this.nameHandler} className="form-control" placeholder="Enter your name.." required/>
                     </div>
 
                     <div className="form-group">
                       <label>Enter Amount</label>
-                      <input type="number" onChange={this.rentHandler} className="form-control" placeholder="Enter rent amount.."/>
+                      <input type="number" onChange={this.rentHandler} className="form-control" placeholder="Enter rent amount.." required/>
                     </div>
 
                     <div className="form-group">
@@ -137,21 +153,23 @@ class newPost extends Component{
 
                     <div className="form-group">
                       <label>Enter Address:</label>
-                      <input type="text" onChange={this.addressHandler} className="form-control" placeholder="Enter address.."/>
+                      <input type="text" onChange={this.addressHandler} className="form-control" placeholder="Enter address.." required/>
                     </div>
 
                     <div className="form-group">
                       <label>Area:</label>
-                      <input type="text" onChange={this.areaHandler} className="form-control" placeholder="Enter area.."/>
+                      <input type="text" onChange={this.areaHandler} className="form-control" placeholder="Enter area.." required/>
                     </div>
 
                     <div className="form-group">
                       <label>Enter Pincode:</label>
-                      <input type="text" onChange={this.pincodeHandler} className="form-control" placeholder="Enter pincode.."/>
+                      <input type="number" onChange={this.pincodeHandler} className="form-control" placeholder="Enter pincode.." required/>
+                      <span className="text-danger position-absolute"> { this.state.ispintrue }</span>
                     </div>
                     <div className="form-group">
                       <label>Phone Number:</label>
-                      <input type="text" onChange={this.phoneHandler} placeholder="Enter phone number.." className="form-control"/>
+                      <input type="number" onChange={this.phoneHandler} placeholder="Enter phone number.." className="form-control"/>
+                      <span className="text-danger position-absolute">{this.state.isphonetrue}</span>
                     </div>
                     
                     <div className="form-group">
@@ -161,12 +179,11 @@ class newPost extends Component{
                       <input type="file" onChange={this.fileHandler3} className="form-control"/>
                       <input type="file" onChange={this.fileHandler4} className="form-control"/>
                     </div>
-                    
-                    <button disabled={uploadBtnDisable} className="Btn" onClick={this.uploadHandler}>upload</button>
+                   
+                    <h6 className="position-absolute text-danger">{this.state.error}</h6>
+                    <button disabled={uploadBtnDisable} className="Btn mt-4" onClick={this.uploadHandler}>upload</button>
                        <input type="submit" className="submitBtn ml-5" onClick={this.submitHandler} value="submit"/>
-                    <p className="text-danger">{this.state.error}</p>
-                    
-
+                       </form>
                  </div>
             </div>
         );
