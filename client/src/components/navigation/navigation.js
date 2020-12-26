18c015f5bd6ef5e616fbf154386de5cc0a38e684
Asmit shrivastava <asmitshrivastava8@gmail.com>
2020-12-26 18:50:09 +0530

    docs: change nav bar

--- client/src/components/navigation/navigation.js
+++ client/src/components/navigation/navigation.js
@@ -1,42 +1,51 @@
 import React,{ Component } from 'react';
-import { Link } from 'react-router-dom';
 import fire from '../../Authorization/auth';
 import './navigation.css';
-import {withRouter} from 'react-router-dom'
+import {Link, withRouter} from 'react-router-dom';
+import Backdrop from '../backdrop/backdrop'; 
 
 class Nav extends Component{
+
+  state = {
+    show: false
+  }
 
    signoutHandler = () => {
      fire.auth().signOut();
      this.props.history.push('/');
    }
 
+   toggleHandler = () => {
+     this.setState({ show: !this.state.show })
+   }
+  
+
   render(){
     return(
         <div> 
-            <nav className="navbar navbar-expand-md p-3">
-
-              <Link className="navbar-brand" to='/'>Home</Link>
-    
-              <ul className="navbar-nav ml-auto">
-                 <li className="nav-item">
-                    <Link className="nav-link" to='/about'>{this.props.type === 'About'? <h3>About</h3> : 'About' }</Link>
-                 </li>
-                 <li className="nav-item">
-                    <Link className="nav-link" to='/work'>{this.props.type === 'Work'? <h3>Work With Us</h3> : 'Work with us' }</Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to='/contact'>{this.props.type === 'Contact'? <h3>Contact</h3> : 'Contact' }</Link>
-                  </li>
-                  <li className="nav-item">
-                    <button className="btn btn-color mt-1" onClick={this.signoutHandler}><h5>Signout</h5></button>
-                  </li>
-              </ul>
-           </nav>
-           
+             <Backdrop show={this.state.show}/>
+	               <input type="checkbox" id="check"/>
+	               <label htmlFor="check">
+		                <i className="fa fa-bars" id="btn" onClick={this.toggleHandler}></i> 
+		                <i className="fa fa-times" id="cancel" onClick={this.toggleHandler}></i>
+	               </label>
+             <div className="side">
+		             <header>MY MENU</header>
+	               <Link to="/" className= "mainnav"><i className="fa fa-home"></i> HOME</Link>
+	               <Link to="/about"><i className="fa fa-info-circle"></i> ABOUT</Link>
+	               <Link to="/work"><i className="fa fa-wrench"></i> WORK WITH US</Link>
+	               <Link to="/contact"><i className="fa fa-phone"></i> CONTACT</Link>
+	               <Link to="" onClick={this.signoutHandler}><i className="fa fa-arrow-right"></i> SIGNOUT</Link>
+             </div>
+              <div className="topnav">
+                	<Link to="/"><i className="fa fa-home"></i></Link>
+                	<Link to="/newpost"><i className="fa fa-plus-square"> POST</i></Link>
+             	</div> 
         </div>
     );
   };
 };
 
 export default withRouter(Nav);
+
+
