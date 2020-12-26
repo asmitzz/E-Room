f61d37382b071637d691aec154ad1a932d13fb0a
Asmit shrivastava <asmitshrivastava8@gmail.com>
2020-12-26 18:51:54 +0530

    remove add post

--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -1,17 +1,16 @@
 import React,{ Component } from 'react';
 import Nav from '../navigation/navigation';
 import Posts from './posts/posts';
-import { Link } from 'react-router-dom';
 class Home extends Component{
   render(){
     return(
         <div> 
            <Nav/>
-               <div>
-                    <Link to="/newpost" className="mx-4 p-3 text-white border border-light rounded">
+               {/* <div>
+                    <Link to="/newpost" className="m-5 p-3 text-white border border-light rounded">
                     <img alt="plus" src="https://img.icons8.com/emoji/48/000000/plus-emoji.png" />
                     Add Post</Link>
-               </div>
+               </div> */}
            <Posts/>
         </div>
     );
