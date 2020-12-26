c647ad5853dc3b9036c9d98a1480a143326708f2
Asmit shrivastava <asmitshrivastava8@gmail.com>
2020-12-26 18:48:24 +0530

    change logo

--- client/src/components/navigation/navigationItems/Contact/contact.js
+++ client/src/components/navigation/navigationItems/Contact/contact.js
@@ -2,38 +2,38 @@ import React,{ Component } from 'react';
 import '../../../navigation/navigation.css';
 import './contact.css';
 import Nav from '../../navigation';
+import logo from '../../../../assets/Images/contact.svg';
 
 class Contact extends Component{
   render(){
     return(
         <div> 
-           <Nav type="Contact"/>
-           <div className="contact">
-           </div>
+           <Nav/>
+           <img src={logo} alt="contact" className="w-100 mt-5"/>
            <section className="main-section">
                <h2 className="contactUs">CONTACT US</h2>
-               <i class="fas fa-phone-alt">&nbsp; 1-80-00330808</i>
-               <i class="pl-5 fas fa-envelope">&nbsp;brandname@gmail.com</i>
+               <i className="fas fa-phone-alt">&nbsp; 1-80-00330808</i>
+               <i className="pl-5 fas fa-envelope">&nbsp;brandname@gmail.com</i>
                <hr/>
                <strong>Fancy working together or just say hi? Drop us a message below.</strong>
                <form className="mt-3">
                  <div>
-                   <label>Your Name</label>
+                   <label className="contact-label">Your Name</label>
                    <input className="form-control" type="text" placeholder="Enter your name.."/>
                  </div>
 
                  <div>
-                   <label>Your Email</label>
+                   <label className="contact-label">Your Email</label>
                    <input className="form-control" type="text" placeholder="Enter your email.."/>
                  </div>
 
                  <div>
-                   <label>Subject</label>
+                   <label className="contact-label">Subject</label>
                    <input className="form-control" type="text" placeholder="What would you like to talk us about?"/>
                  </div>
 
                  <div>
-                   <label>Message</label>
+                   <label className="contact-label">Message</label>
                    <textarea className="form-control" type="text" placeholder="Type away :)"></textarea>
                  </div>
 
