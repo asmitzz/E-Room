5c7808177166934fca9a8a086cb84060f479be54
Asmit shrivastava <asmitshrivastava8@gmail.com>
2021-01-03 18:23:09 +0530

    feat: added pincode suggestions

--- client/src/components/filter/filter.js
+++ client/src/components/filter/filter.js
@@ -7,8 +7,9 @@ class Filter extends Component{
     state = {
        posts: "",
        location:"",
+       locationSuggest:"",
        pincode:"",
-       area:""
+       pincodeSuggest:""
     };
 
     componentDidMount = () => {
@@ -18,12 +19,17 @@ class Filter extends Component{
             })
         });
 
-        window.addEventListener('click',()=>this.setState({area:""}))
+        window.addEventListener('click',() => this.setState({locationSuggest:"",pincodeSuggest:""}));
     }
 
-    searchHandler = (area) => {
+    areaSuggestHandler = (area) => {
         this.setState({ location:area });
-        this.setState({area:""})
+        this.setState({locationSuggest:""});
+    }
+
+    pinSuggestHandler = (pin) => {
+        this.setState({ pincode: pin });
+        this.setState({ pincodeSuggest:""});
     }
 
     areaHandler = (e) => {
@@ -40,16 +46,35 @@ class Filter extends Component{
         const result = area.filter( area => area.includes(e.target.value.toLowerCase()));
 
         if( e.target.value ){
-            this.setState({ area:result.map((area) => {
-                return <span className="highlight" onClick={()=>this.searchHandler(area)} key={area}>{area.toUpperCase()}</span>
+            this.setState({ locationSuggest:result.map((area) => {
+                return <span className="highlight" onClick={() => this.areaSuggestHandler(area)} key={area}>{area.toUpperCase()}</span>
             } ) })
         }
         else{
-            this.setState({ area:"" })
+            this.setState({ locationSuggest:"" })
         }
     }
+    
     pincodeHandler = (e) => {
-        this.setState({ pincode: e.target.value })
+        this.setState({ pincode: e.target.value });
+        const pincode =  Object.keys(this.state.posts).map((post)=>{
+            return this.state.posts[post].pincode.toString();
+        });
+  
+          const pincodeWithoutRepeat = new Set(pincode);
+             
+          const pincodes = [...pincodeWithoutRepeat];
+  
+          const result = pincodes.filter( pin => pin.includes(e.target.value));
+  
+          if( e.target.value ){
+              this.setState({ pincodeSuggest : result.map((pin) => {
+                  return <span className="highlight" onClick={() => this.pinSuggestHandler(pin)} key={pin} >{pin}</span>
+              } ) })
+          }
+          else{
+              this.setState({ pincodeSuggest:"" })
+          }
     }
 
     ApplyBtnHandler = () => {
@@ -65,9 +90,10 @@ class Filter extends Component{
                 <h2 className="text-center"><i className="fa fa-filter"></i>Filter</h2>
                 <label className="mt-5 d-inline-block">Location: &nbsp;</label>
                 <input type="text" className="form-control" onChange={this.areaHandler} value={this.state.location} />
-                <span className="areaSuggestions">{this.state.area}</span>
+                <span className="areaSuggestions">{this.state.locationSuggest}</span>
                 <label className="mt-2 d-inline-block">Pincode: &nbsp;</label>
-                <input type="number" className="form-control" onChange={this.pincodeHandler}/>
+                <input type="number" className="form-control" onChange={this.pincodeHandler} value={this.state.pincode}/>
+                <span className="pinSuggestions">{this.state.pincodeSuggest}</span>
                 <button className="btn mt-3 w-50" onClick={this.ApplyBtnHandler}>Apply</button>
               </div>
           </div>
